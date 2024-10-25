ddffcb9d6e07f5e0a12d2a84a78269d2aa8476a7
han18 <han.fadah@gmail.com>
2024-10-25 11:08:21 -0400

    working with the default method

---
+++ index.js
@@ -0,0 +1,24 @@
+/// exporting and importing files 
+
+
+export function sayHello() {
+    console.log("Hello this a module lab practice to import and export modules")
+
+}
+
+sayHello();
+
+export default function callMe() {
+    alert("This is an alert")
+}
+
+callMe()
+
+export default function monthOfYear() {
+    let months = ['Jan', 'Feb', 'Mar','Apr', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    return months;
+
+}
+
+monthOfYear();
+
---
+++ main.js
@@ -0,0 +1,7 @@
+import { sayHello } from './index.js';
+import callMe from './index.js'; // using default
+import months  from './index.js';
+
+console.log(sayHello)
+console.log(callMe)
+console.log(months)
